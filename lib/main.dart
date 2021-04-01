6576ed8f34e740ecfa790570b41952b6cb09b61d
Your Name <you@example.com>
2021-04-01 18:57:09 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -10,42 +10,66 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "App 1",
       home: Scaffold(
-        backgroundColor: Colors.yellowAccent,
-        appBar: AppBar(
-          title: Text("Container Widget"),
-          leading: Icon(Icons.home),
-          backgroundColor: Colors.redAccent,
-        ),
-        body: Column(
-          //you can also write Column for column container
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.amberAccent,
-              margin: EdgeInsets.all(10),
-              child: Text("hey coder"),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.amberAccent,
-              margin: EdgeInsets.all(10),
-              child: Text("second wigits"),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.amberAccent,
-              margin: EdgeInsets.all(10),
-              child: Text("hey coder"),
-            ),
-          ],
-        ),
-      ),
+          backgroundColor: Colors.amberAccent,
+          appBar: AppBar(
+            title: Text("Day 4"),
+            leading: Icon(Icons.home),
+            backgroundColor: Colors.redAccent,
+          ),
+          body: Column(
+            children: [
+              Text(
+                "hello coder !! this is my name is mrinal sharma what about you",
+                style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700,
+                    backgroundColor: Colors.blueAccent,
+                    letterSpacing: 2.0,
+                    wordSpacing: 15.0,
+                    shadows: [
+                      Shadow(
+                        color: Colors.lightBlueAccent,
+                        offset: Offset(3.0, 2.0),
+                      ),
+                    ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Hello!!",
+                      style: TextStyle(
+                        color: Colors.greenAccent,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'coder',
+                          style: TextStyle(
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ]),
+                ),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      // obscureText: true,//hide your input text as like password
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Name :',
+                        hintText: 'Enter Your Name',
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
