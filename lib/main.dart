f9ef8a8d6b343948885296c0e15b00cc8e4dbcb8
Your Name <you@example.com>
2021-04-01 17:04:24 +0530

    3rd Day

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "App 1",
       home: Scaffold(
-          backgroundColor: Colors.yellowAccent,
-          appBar: AppBar(
-            title: Text("Container Widget"),
-            leading: Icon(Icons.home),
-            backgroundColor: Colors.redAccent,
-          ),
-          body: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(10),
-            alignment: Alignment.center,
-            child: Text("hey Codder !!"),
-            decoration: BoxDecoration(
-                border: Border.all(width: 4, color: Colors.black),
-                color: Colors.greenAccent,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.orangeAccent,
-                    offset: Offset(6.0, 6.0),
-                  )
-                ]),
-            transform: Matrix4.rotationZ(0.1),
-          )),
+        backgroundColor: Colors.yellowAccent,
+        appBar: AppBar(
+          title: Text("Container Widget"),
+          leading: Icon(Icons.home),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          //you can also write Column for column container
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amberAccent,
+              margin: EdgeInsets.all(10),
+              child: Text("hey coder"),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amberAccent,
+              margin: EdgeInsets.all(10),
+              child: Text("second wigits"),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amberAccent,
+              margin: EdgeInsets.all(10),
+              child: Text("hey coder"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
