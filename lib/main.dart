923e93fb6fa3b4c222f4331d84e20a31e4f97672
Your Name <you@example.com>
2021-04-06 07:45:10 +0530

    7 days learn about stack and position

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,128 +10,121 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'my App',
+      title: 'my android',
       home: Scaffold(
-          backgroundColor: Colors.grey,
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
-            leading: Icon(Icons.home),
-            backgroundColor: Colors.redAccent,
-          ),
-          body: Center(
-            child: Column(
-              children: [
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    margin: EdgeInsets.only(top: 40),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: Colors.lightBlueAccent,
-                        border: Border.all(color: Colors.white, width: 2)),
-                    child: Center(
-                        child: Text(
-                      'M',
-                      style: TextStyle(
-                          fontSize: 50,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic),
-                    )),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(30),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Mrinal Sharma',
-                        icon: Icon(Icons.person, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 30, right: 30),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                        icon: Icon(Icons.lock, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 30, top: 25, right: 30),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Github Link',
-                        icon: Icon(Icons.link, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                //flat button
-                FlatButton(
+        appBar: AppBar(
+          title: Text('Hello '),
+          leading: Icon(Icons.home),
+        ),
+        body: Center(
+          child: Stack(
+            fit: StackFit.passthrough,
+            overflow: Overflow.visible,
+            children: [
+              Container(
+                height: 300,
+                width: 400,
+                color: Colors.green,
+                child: Center(
                   child: Text(
-                    'Update',
+                    "hello !!!",
                     style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.italic),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                  onPressed: () {},
+                ),
+              ),
+              Positioned(
+                top: 20,
+                right: 30,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                      "coders.",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.lightBlue,
+                  child: Center(
+                    child: Text(
+                      "coders.",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 20,
+                bottom: 20,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                      "coders.",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 20,
+                child: Container(
+                  height: 100,
+                  width: 100,
                   color: Colors.lightBlueAccent,
-                  textColor: Colors.white,
+                  child: Center(
+                    child: Text(
+                      "coders.",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
                 ),
-                //raise button
-                RaisedButton(
-                  child: Text('Updated'),
-                  onPressed: () {},
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  splashColor: Colors.lightGreenAccent,
+              ),
+              Container(
+                height: 50,
+                width: 400,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    "hello !!!",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
-                //floating action button
-                FloatingActionButton(
-                  child: Icon(Icons.save),
-                  // Text('Submit'),
-                  onPressed: () {},
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.yellow,
-                ),
-                FloatingActionButton.extended(
-                  onPressed: () {},
-                  label: Text('Save'),
-                  icon: Icon(Icons.save),
-                ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
