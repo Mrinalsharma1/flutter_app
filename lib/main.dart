1cf2fdca5b694450e11f1ee1bf2ba161112dd492
Your Name <you@example.com>
2021-04-12 23:24:48 +0530

    11 days card_widgit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,346 +10,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter With Mrinal',
+      title: '11 days',
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.home),
-          title: Text('Icon & Images'),
-          backgroundColor: Colors.lightGreen,
+          leading: Icon(
+            Icons.home,
+            size: 30,
+            color: Colors.blueAccent,
+          ),
+          title: Text(
+            '11 days Card',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.amber,
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+          child: Center(
+            child: Container(
+              height: 200,
+              width: 250,
+              // padding: EdgeInsets.all(10),
+              child: Card(
+                color: Colors.amberAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                elevation: 20,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.home,
-                              color: Colors.black,
-                              size: 40,
-                              textDirection: TextDirection.rtl,
-                            ),
-                            Text('Home'),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.access_alarm,
-                          color: Colors.blueAccent,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.person,
                           size: 40,
-                          textDirection: TextDirection.ltr,
+                          color: Colors.black87,
                         ),
-                        Text('Alarm'),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.edit_road,
-                          color: Colors.redAccent,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
+                        title: Text(
+                          'Mrinal Shamra',
+                          style:
+                              TextStyle(fontSize: 20, color: Colors.blueAccent),
                         ),
-                        Text('Road'),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.mail,
-                          color: Colors.blueGrey,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
+                        subtitle: Text(
+                          'Name',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey,
+                          ),
                         ),
-                        Text('Road'),
-                      ],
+                      ),
                     ),
-                  ],
-                ),
-
-                // this is third grid
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
+                    ButtonBar(
                       children: [
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.cancel,
-                              color: Colors.redAccent,
-                              size: 40,
-                              textDirection: TextDirection.rtl,
-                            ),
-                            Text('Cancle'),
-                          ],
+                        // ignore: deprecated_member_use
+                        RaisedButton(
+                          child: FloatingActionButton.extended(
+                            onPressed: () {},
+                            label: Text('Contact'),
+                            icon: Icon(Icons.call),
+                          ),
+                          // color: Colors.redAccent,
+                          onPressed: () {},
                         ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.delete,
-                          color: Colors.orangeAccent,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
-                        ),
-                        Text('Info'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.file_copy,
-                          color: Colors.black,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
-                        ),
-                        Text('Image'),
-                      ],
-                    ),
-                  ],
-                ),
-
-                // our image section with grid
-
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Image.asset(
-                              "image/1.jpg",
-                              height: 100,
-                              width: 150,
-                            ),
-                            Text("My First Image"),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Image.asset(
-                              "image/1.jpg",
-                              height: 100,
-                              width: 150,
-                            ),
-                            Text("My Second Image"),
-                          ],
+                        RaisedButton(
+                          child: Text('Submit'),
+                          color: Colors.lightBlue,
+                          onPressed: () {},
                         ),
                       ],
                     ),
                   ],
                 ),
-
-                // this is  also a another container of image where we are fetch image from image link to google
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Image.network(
-                              "https://images.pexels.com/photos/36762/scarlet-honeyeater-bird-red-feathers.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                              height: 100,
-                              width: 150,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: Text("This is network image"),
-                                decoration: BoxDecoration(
-                                  color: Colors.greenAccent,
-                                  borderRadius: BorderRadius.circular(3),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.orangeAccent,
-                                      offset: Offset(2.0, 3.0),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Image.network(
-                              "https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                              height: 100,
-                              width: 150,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: Text("Image From Google"),
-                                decoration: BoxDecoration(
-                                  color: Colors.amberAccent,
-                                  borderRadius: BorderRadius.circular(3),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.blue,
-                                      offset: Offset(2.0, 3.0),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-
-                // here we are show fade image property in there if we are not connect to network then placeholder image are shown insted of
-                // our actual image.
-
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            FadeInImage.assetNetwork(
-                              placeholder: 'image/1.jpg',
-                              image:
-                                  "https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-                              height: 100,
-                              width: 150,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text("Placeholder Image"),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            FadeInImage.assetNetwork(
-                              placeholder: 'image/1.jpg',
-                              image:
-                                  "https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-                              height: 100,
-                              width: 150,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text("Placeholder Image"),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-
-                //this is last section of icons widgt
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.search,
-                              color: Colors.blueAccent,
-                              size: 40,
-                              textDirection: TextDirection.rtl,
-                            ),
-                            Text('Search'),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.info,
-                          color: Colors.black,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
-                        ),
-                        Text('Info'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.image,
-                          color: Colors.redAccent,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
-                        ),
-                        Text('Image'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.folder,
-                          color: Colors.blueGrey,
-                          size: 40,
-                          textDirection: TextDirection.ltr,
-                        ),
-                        Text('Folder'),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
         ),
