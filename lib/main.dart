14e636ea167779f95883c156ea2deadc8554fb9b
Your Name <you@example.com>
2021-04-23 08:36:49 +0530

    day17 long list

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Newcard.dart';
 
 void main() {
-  runApp(MyApp(names: List<String>.generate(50, (a) => "coder!! $a")));
+  // runApp(MyApp(names: List<String>.generate(50, (a) => "coder!! $a")));
 }
 
 class MyApp extends StatelessWidget {
-  final List<String> names;
-  MyApp({key, this.names}) : super(key: key);
+  // final List<String> names;
+  // MyApp({key, this.names}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,14 +17,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Long list"),
         ),
-        body: ListView.builder(
-            itemCount: names.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text("Hii ${names[index]}"),
-              );
-            }),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(datas.length, (index) {
+            return Center(
+              child: Newcard(
+                datas: datas[index],
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
 }
+
+class data {
+  const data({this.title, this.icon});
+  final String title;
+  final IconData icon;
+}
+
+const List<data> datas = const <data>[
+  const data(title: 'Home', icon: Icons.home),
+  const data(title: 'Home', icon: Icons.add_box),
+  const data(title: 'Home', icon: Icons.home),
+  const data(title: 'Setting', icon: Icons.settings),
+  const data(title: 'Home', icon: Icons.home),
+  const data(title: 'Home', icon: Icons.home),
+];
