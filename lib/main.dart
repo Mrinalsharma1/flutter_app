8e2905efeb02c08a90d422653371dc754295926f
Your Name <you@example.com>
2021-05-07 07:03:40 +0530

    21_ days material table

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _DataTableExample createState() => _DataTableExample();
+}
+
+class _DataTableExample extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: Switch_wigit(),
-      home: table_wigit(),
-    );
-  }
-}
-
-class Switch_wigit extends StatefulWidget {
-  @override
-  _Switch_wigitState createState() => _Switch_wigitState();
-}
-
-class _Switch_wigitState extends State<Switch_wigit> {
-  bool switch_state = false;
-  var textValue = "switch is on";
-  void display_state(bool value) {
-    if (switch_state == false) {
-      setState(() {
-        switch_state = true;
-        textValue = "switch is on";
-      });
-    } else {
-      setState(() {
-        switch_state = false;
-        textValue = "switch is off";
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("switch"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: Transform.scale(
-                scale: 2,
-                child: Switch(
-                  value: switch_state,
-                  onChanged: display_state,
-                  activeColor: Colors.green,
-                  inactiveTrackColor: Colors.red,
-                  inactiveThumbColor: Colors.redAccent,
-                ),
-              ),
-            ),
-            Text('$textValue')
-          ],
-        ));
-  }
-}
-
-class table_wigit extends StatefulWidget {
-  @override
-  _table_wigitState createState() => _table_wigitState();
-}
-
-class _table_wigitState extends State<table_wigit> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Table_wigit"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(5),
-        child: Table(
-          defaultColumnWidth: FixedColumnWidth(140),
-          border: TableBorder.all(
-            color: Colors.red,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter DataTable Example'),
           ),
-          children: [
-            TableRow(children: [
-              Column(
-                children: [
-                  Text(
-                    "Name",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Text(
-                    "Name",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Text(
-                    "Name",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-            ]),
-            TableRow(children: [
-              Column(
-                children: [
-                  Text("Class"),
-                ],
-              ),
-              Column(
-                children: [
-                  Text("Name"),
-                ],
-              ),
-              Column(
-                children: [
-                  Text("Name"),
-                ],
-              ),
-            ]),
-            TableRow(children: [
-              Column(
-                children: [
-                  Text("Subject"),
-                ],
-              ),
-              Column(
-                children: [
-                  Text("Name"),
-                ],
-              ),
-              Column(
-                children: [
-                  Text("Name"),
-                ],
-              ),
-            ]),
-          ],
-        ),
-      ),
+          body: ListView(children: <Widget>[
+            Center(
+                child: Text(
+              'People-Chart',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            )),
+            DataTable(
+              columns: [
+                DataColumn(
+                    label: Text('ID',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Name',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Profession',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('1')),
+                  DataCell(Text('Stephen')),
+                  DataCell(Text('Actor')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('5')),
+                  DataCell(Text('John')),
+                  DataCell(Text('Student')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('10')),
+                  DataCell(Text('Harry')),
+                  DataCell(Text('Leader')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('15')),
+                  DataCell(Text('Peter')),
+                  DataCell(Text('Scientist')),
+                ]),
+              ],
+            ),
+          ])),
     );
   }
 }
